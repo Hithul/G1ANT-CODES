befa53fa2fd3ceddf81877ba02fcffd8449096cd
Hithul <65595632+Hithul@users.noreply.github.com>
2020-06-30 23:39:53 +0530

    Add files via upload

---
+++ TheADDONCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G1ANT.Language;
+
+namespace G1ANT.Addon.MyAddon
+{
+    [Command(Name ="mycommand", Tooltip = "This is a demo command.")]
+    class MyCommand : Command
+    {
+        public MyCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+        public class Arguments : CommandArguments
+        {
+            [Argument(Name = "message", Required = true, Tooltip = "Type in the text to display")]
+            public TextStructure message { get; set; } = new TextStructure();
+
+        }
+        public void Execute(Arguments arguments)
+        {
+            RobotMessageBox.Show(arguments.message.Value);
+        }
+    }
+}
